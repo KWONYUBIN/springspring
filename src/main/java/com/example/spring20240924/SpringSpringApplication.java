6b2b302e24e7c81a5919c97a4e70af80027b1d9f
KWONYUBIN <71390019+KWONYUBIN@users.noreply.github.com>
2024-09-25 16:09:48 +0900

    0925

--- src/main/java/com/example/spring20240924/SpringSpringApplication.java
+++ src/main/java/com/example/spring20240924/SpringSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring20240924Application {
+public class SpringSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring20240924Application.class, args);
+        SpringApplication.run(SpringSpringApplication.class, args);
     }
 
 }
